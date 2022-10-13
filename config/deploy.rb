7878a80d7cbe2ecf44d3ff2b54549230cad460cd
Josh Hawks <snl95@txstate.edu>
2022-10-13 13:40:51 -0500

    adding linked files & dirs 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/var/www/#{fetch(:application)}"
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", 'config/master.key'
 
-set :linked_files, %w[config/master.key]
+set :linked_files, %w[config/credentials.yml.enc]
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
