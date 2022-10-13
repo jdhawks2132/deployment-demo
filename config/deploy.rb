ee552ca91966f40566aad99bc387c7fb0ea0c613
Josh Hawks <snl95@txstate.edu>
2022-10-13 15:02:56 -0500

    uploaded db.yml to server x 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,8 @@ set :deploy_to, "/var/www/#{fetch(:application)}"
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", 'config/master.key'
 
-append :linked_files, ['config/credentials/production.key', 'config/database.yml']
+set :linked_files, %w[config/database.yml]
+append :linked_files,  'config/credentials/production.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
