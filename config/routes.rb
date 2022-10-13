60b321d48d11795b5789d22a890fd086839b786f
Josh Hawks <snl95@txstate.edu>
2022-10-13 15:36:24 -0500

    pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about'
+  
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
