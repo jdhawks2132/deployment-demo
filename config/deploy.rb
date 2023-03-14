8ef8dbcbb95f64fd065af0461e0e366da99daaa2
Josh Hawks <snl95@txstate.edu>
2023-03-14 11:03:29 -0500

    updates to puma.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
+lock '~> 3.17.1'
 
-set :application, "deployment-demo"
-set :repo_url, "git@github.com:jdhawks2132/deployment-demo.git"
+set :application, 'deployment-demo'
+set :repo_url, 'git@github.com:jdhawks2132/deployment-demo.git'
 set :branch, 'main'
 
 # Default branch is :master
@@ -25,11 +25,46 @@ set :deploy_to, "/var/www/#{fetch(:application)}"
 # append :linked_files, "config/database.yml", 'config/master.key'
 
 set :linked_files, %w[config/database.yml]
-append :linked_files,  'config/credentials/production.key'
+append :linked_files, 'config/credentials/production.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
 set :linked_dirs, %w[log tmp/pids tmp/sockets public/uploads public/assets]
+
+set :puma_bind,
+    "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log, "#{release_path}/log/puma.error.log"
+set :puma_role, :web
+
+# Restart Puma
+namespace :puma do
+  desc 'Restart application'
+  task :restart do
+    invoke 'puma:stop'
+    invoke 'puma:start'
+  end
+
+  desc 'Stop application'
+  task :stop do
+    on roles(fetch(:puma_role)) do
+      execute "if [ -e #{fetch(:puma_pid)} ] && kill -0 `cat #{fetch(:puma_pid)}`> /dev/null 2>&1; then kill -QUIT `cat #{fetch(:puma_pid)}`; fi"
+    end
+  end
+
+  desc 'Start application'
+  task :start do
+    on roles(fetch(:puma_role)) do
+      within current_path do
+        execute :bundle, :exec, :puma, "-C #{release_path}/config/puma.rb"
+      end
+    end
+  end
+end
+
+after 'deploy:publishing', 'puma:restart'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
--- config/puma.rb
+++ config/puma.rb
@@ -13,10 +13,6 @@ threads min_threads_count, max_threads_count
 #
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port ENV.fetch("PORT") { 3000 }
-
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
@@ -30,14 +26,17 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers 2
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+# Bind Puma to a TCP socket at port 3000
+bind "tcp://0.0.0.0:3000"
